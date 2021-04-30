ae959df73c805556637872023f4c5a069b48a929
Fredins <fredin.martin@gmail.com>
2021-04-30 11:53:22 +0200

    gitignore

--- config.h
+++ config.h
@@ -2,12 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int gappx     = 0;        /* gaps between windows */
+static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "iosevka:pixelsize=10:antialias=true:autohint=true" };
-static const char dmenufont[]       = "iosevka:pixelsize=14:antialias=true:autohint=true";
+static const char *fonts[]          = { "SometypeMono-Regular:pixelsize=10:antialias=true:autohint=true" };
+static const char dmenufont[]       = "SometypeMono-Regular:pixelsize=14:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -18,7 +18,7 @@ static const char col_pink[]        = "#ffb7c5";
 static const char col_black[]       = "#000000"; 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm]  = { col_gray3, col_gray1,  col_darkblue  },
+	[SchemeNorm]  = { col_gray3, col_gray1,  col_black  },
 	[SchemeSel]  = { col_gray4, col_darkblue,  col_pink  },
 };
 
@@ -38,7 +38,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -89,8 +89,8 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_plus,  setgaps,         {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = 0  } },
+	{ MODKEY,                       XK_plus,  setgaps,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
